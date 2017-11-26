6e8cd24d344bbf4b336a24e7869f914f63f01853
Jake Johnson <jakejohnson@jims-MacBook-Pro.local>
2017-11-25 18:18:11 -0700

    Fixed WebViewController build attachement, disabled landscape mode

--- PhET_Intern_Task/WebViewController.swift
+++ PhET_Intern_Task/WebViewController.swift
@@ -13,30 +13,34 @@ import WebKit //needed for webView component
 class WebViewController: UIViewController, WKUIDelegate { //WKUIDelegate needed for WKWebView
     
     var webView: WKWebView! //create WKWebView object
-    
+
     //Programmatically add a WKView component
     override func loadView() {
         let webConfiguration = WKWebViewConfiguration() //create WKWebViewConfig object
         webView = WKWebView(frame: .zero, configuration: webConfiguration) //set webView
         webView.uiDelegate = self
+        //webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         view = webView //set view of this controller to webView
         self.navigationController?.interactivePopGestureRecognizer?.isEnabled = false
         //disable the swiping navigation function since it interfers with simulation
-    }
+        webView.evaluateJavaScript("window.scrollBy(1, 1);window.scrollBy(-1, -1);", completionHandler: nil)
 
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         //Set url to the simulation
         let myURL = URL(string: "https://phet.colorado.edu/sims/html/circuit-construction-kit-dc/latest/circuit-construction-kit-dc_en.html")
         let myRequest = URLRequest(url: myURL!) //create a URLRequest to the simulation
         webView.load(myRequest) //load the request to the view
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 }
+
